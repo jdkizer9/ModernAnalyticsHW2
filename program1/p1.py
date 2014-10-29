f64dad921a6ef777e8d805b1cc6a1e6bca19a537
James <james.kizer@gmail.com>
2014-10-29 11:24:53 -0400

    Working problem 1

--- program1/p1.py
+++ program1/p1.py
@@ -9,9 +9,11 @@ from config import S_FIELDS,F_FIELDS,EXAMPLE_DATA,TRIP_DATA_1,TRIP_DATA_2, TRAIN
 import code.utils as utils
 from code.distance import get_distance
 from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors.kde import KernelDensity
 import math
 from scipy import stats
 from datetime import datetime, date, time
+import matplotlib.pyplot as plt
 
 
 ##return a function that takes in a filename and a number of rows and returns two generator
@@ -113,6 +115,11 @@ def composeFunctions(functions):
 	newFunctions.reverse()
 	return reduce(lambda f, g: lambda x: f(g(x)), newFunctions)
 
+def composeFilters(filters):
+	newFilters = list(filters)
+	newFilters.reverse()
+	return reduce(lambda f, g: lambda x: f(x) and g(x), newFilters)
+
 #FEATURES: [pickup_time, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, trip_distance]
 def derive_filter(rows,tolerance = 4.0):
     """
@@ -138,6 +145,62 @@ def derive_filter(rows,tolerance = 4.0):
         # print 'Filtering {0}: Distance: {1} Mean: {2} STD: {3}'.format(row, get_distance(plat,plong,dlat,dlong), trip_dist_mean, trip_dist_std)
         return False
     return custom_filter
+
+def derive_BadDataFilter(indexesToFilter):
+	def custom_filter(row):
+		#print indexesToFilter
+		for index in indexesToFilter:
+			if row[index] == 0.:
+				#print 'filtered As Bad Data'
+				#print index
+				#print row
+				return False
+		return True
+	return custom_filter
+
+def NYCFilter(row):
+	if (-74.1 < row[1] < -73.7) and (40.5 < row[2] < 40.9):
+		return True
+	else:
+		return False
+
+def derive_meanDevFilter(rows, indexesToFilter, tolerance=4.0):
+
+	means,stdDevs = {},{}
+	#print indexes
+	array = np.array([row for row in rows])
+	for index in indexesToFilter:
+		featureArray = array[:,index]
+		#print np.amin(featureArray)
+		#print np.amax(featureArray)
+		means[index] = np.mean(featureArray)
+		stdDevs[index] = np.std(featureArray)
+
+	#print means
+	#print stdDevs
+
+	def custom_filter(row):
+		#print indexesToFilter
+		for index in indexesToFilter:
+			#print math.fabs(row[index]-means[index])/stdDevs[index]
+			if math.fabs(row[index]-means[index])/stdDevs[index] > tolerance:
+				# print 'Filtered'
+				# print row
+				return False
+		return True
+	return custom_filter
+
+
+#FEATURES: [passenger_count, dropoff_longitude, dropoff_latitude]
+def passengerFilter(passengers):
+
+
+	def custom_filter(row):
+		if row[0] == passengers and row[1] != 0. and row[2] != 0.:
+			return True
+		else:
+			return False
+	return custom_filter
 
 
 
@@ -238,6 +301,151 @@ def printResults(rms, R, mae):
 	print 'Corr Coef: {0}'.format(R)
 	print 'MAE: {0}'.format(mae)
 
+# def estimateBandwidth(data):
+
+# 	stdDev = np.std(distances)
+#     trip_dist_std = 
+#     trip_dist_std = np.std(distances)
+
+def bucketArray(data, xmin, xmax, ymin, ymax, bucketsPerDim=100):
+	deltaX = (xmax-xmin)/bucketsPerDim
+	deltaY = (ymax-ymin)/bucketsPerDim
+	#print xmin, xmax, deltaX
+	#print ymin, ymax, deltaY
+	def bucketMap(point):
+		#print point
+		xBucket = int(round((point[0]-xmin)/deltaX))
+		if xBucket == bucketsPerDim:
+			xBucket = bucketsPerDim-1
+		yBucket = int(round((point[1]-ymin)/deltaY))
+		if yBucket == bucketsPerDim:
+			yBucket = bucketsPerDim-1
+		return (xBucket, yBucket)
+
+	buckets = np.zeros((bucketsPerDim, bucketsPerDim))
+
+	for point in data:
+		bucket = bucketMap(point)
+		#print bucket
+		buckets[bucket] = buckets[bucket] + 1.
+
+	#print buckets
+	return buckets
+
+
+#this takes WAY too long
+def plotHeatMapForNumberOfPassengers(data, numberOfPassengers, numberOfBucketsPerDim=100):
+
+
+	minima = np.amin(data, axis=0)
+	maxima = np.amax(data, axis=0)
+
+	xMin = minima[1]
+	xMax = maxima[1]
+
+	yMin = minima[2]
+	yMax = maxima[2]
+
+	passengerData = [row[1:3] for row in data if row[0] == numberOfPassengers]
+	#passengerData = [row[1:3] for row in data]
+
+	#data is scaled by 1/n
+	#array = bucketArray(passengerData, xMin, xMax, yMin, yMax, numberOfBucketsPerDim) / len(passengerData)
+	array = bucketArray(passengerData, xMin, xMax, yMin, yMax, numberOfBucketsPerDim) / len(passengerData)
+
+	#print np.amin(array, axis=0)
+	#print np.amin(array, axis=1)
+
+	#print np.amax(array, axis=0)
+	#print np.amax(array, axis=1)
+
+
+
+
+	#numberOfBuckets = 100
+	# import matplotlib.pyplot as plt
+	# import numpy as np
+	#column_labels = range(10*numberOfBuckets, 10)
+	#print column_labels
+	#row_labels = range(numberOfBuckets)
+	#print row_labels
+	#data = np.random.rand(numberOfBuckets,numberOfBuckets)/10
+	#print data
+	fig, ax = plt.subplots()
+	# heatmap = ax.pcolor(array, cmap=plt.cm.Reds, alpha=0.8)
+	heatmap = ax.pcolor(array, cmap=plt.cm.Reds, edgecolors='k')
+	#print data
+
+	#ax.set_xlim(-10., 10.)
+
+	#legend
+	cbar = plt.colorbar(heatmap)
+	# cbar.ax.set_yticklabels(['0','1','2','>3'])
+	# cbar.set_label('# of contacts', rotation=270)
+
+	# put the major ticks at the middle of each cell, notice "reverse" use of dimension
+	#ax.set_yticks([1,2,3,4,5], minor=False)
+	#ax.set_xticks([19, 20, 21, 22, 23], minor=False)
+
+	dx = (xMax-xMin)/5
+	xlabels = [round(xMin + dx*i, 2) for i in range(6)]
+	ax.set_xticklabels(xlabels, minor=False)
+
+	dy = (yMax-yMin)/5
+	ylabels = [round(yMin + dy*i, 2) for i in range(6)]
+	ax.set_yticklabels(ylabels, minor=False)
+	# ax.set_yticklabels(column_labels, minor=False)
+	#plt.show()
+	fileString = 'passengers{0}HeatMap.png'.format(numberOfPassengers)
+	plt.savefig(fileString)
+	plt.close()
+
+
+
+
+def problem1a():
+
+	#Problem 1A Parameters
+	str_fields = []
+	float_fields = [7, 12, 13]
+
+	badDataFilter = derive_BadDataFilter([0, 1, 2])
+	compositeFilter = composeFilters([badDataFilter, NYCFilter])
+	generatorCreatingFunctionForFilter = createFunctionForGenerator(str_fields, float_fields,row_filter=compositeFilter)
+	mean_dev_filter = derive_meanDevFilter(generatorCreatingFunctionForFilter(TRAIN_DATA), [1,2], tolerance=3)
+
+	compositeFilter = composeFilters([badDataFilter, NYCFilter, mean_dev_filter])
+	generatorCreatingFunction = createFunctionForGenerator(str_fields, float_fields,row_filter=compositeFilter)
+	#training_data = np.array([row for row in generatorCreatingFunction(TRAIN_DATA)])
+	training_data = np.array([row for row in generatorCreatingFunction(TRIP_DATA_1)])
+
+	print len(training_data)
+	plotHeatMapForNumberOfPassengers(training_data, 1, 25)
+	plotHeatMapForNumberOfPassengers(training_data, 3, 25)
+
+	
+
+	
+
+
+	# filters = composeFunctions([passenger1filter, mean_dev_filter])
+	
+	# generatorCreatingFunctionFor1Passenger = createFunctionForGenerator(str_fields, float_fields,row_filter=filters)
+	# passenger1data = np.array([row[1:3] for row in generatorCreatingFunctionFor1Passenger(TRAIN_DATA)])
+
+	# #h1 = estimateBandwidth(passenger1data)
+
+	# print passenger1data
+	# print len(passenger1data)
+	# kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(passenger1data)
+	# print ''
+	#print kde.score_samples(passenger1data)
+
+	#passenger1data = None
+	#plotKernel(passenger1data)
+
+
+
 def problem1b():
 
 	##Problem 1B Parameters
@@ -281,7 +489,7 @@ def problem1c():
 
 ##this problem uses the same features as 1c, but we need to add a more intelligent scaling function
 ##this should return the final scaling function used
-def problem1d():
+def problem1d(useStandardization):
 
 	##Problem 1D Parameters
 	k = 1
@@ -306,7 +514,10 @@ def problem1d():
 	generatorCreatingFunctionForScaleTransform = createFunctionForGenerator(str_fields, float_fields+observed_float_fields,row_filter=mean_dev_filter, row_tranformer=simplePickupTimeTransformer)
 	scale_transform = derive_mean_dev_scale_transform(generatorCreatingFunctionForScaleTransform(TRAIN_DATA), (0, 1, 2, 3, 4, 5))
 
-	transform = composeFunctions([simplePickupTimeTransformer, scale_transform])
+	if useStandardization:
+		transform = composeFunctions([simplePickupTimeTransformer, scale_transform])
+	else:
+		transform = simplePickupTimeTransformer
 	# generatorCreatingFunction= createFunctionForGenerator(str_fields, float_fields+observed_float_fields,row_filter=mean_dev_filter, row_tranformer=transform)
 	# training_data = np.array([row for row in generatorCreatingFunction(TRAIN_DATA)])
 	# print len(training_data)
@@ -348,11 +559,12 @@ def problem1e(mean_dev_filter, transform):
 
 def main():
 	
-
+	# problem1a()
 	problem1b()
-	problem1c()
-	(mean_dev_filter, transform) = problem1d()
-	problem1e(mean_dev_filter, transform)
+	# problem1c()
+	#(mean_dev_filter, transform) = problem1d(True)
+	#(mean_dev_filter, transform) = problem1d(False)
+	# problem1e(mean_dev_filter, transform)
 
 
 	
